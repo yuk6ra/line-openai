6d0dcba6ca256febcd7ac587662976d0e0e32bad
yukimura <yuki.imamura.all@gmail.com>
2023-03-12 18:18:42 +0900

    Add openai

---
+++ utils/openai.py
@@ -0,0 +1,20 @@
+import os
+import openai
+import dotenv
+
+dotenv.load_dotenv()
+
+# Load your API key from an environment variable or secret management service
+openai.api_key = os.getenv("OPEN_AI_KEY")
+
+class OpenAI():
+
+    def get_openai_response(content):
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", 
+            messages=[{"role": "user", "content": content}]
+        )
+
+        openai_content = response['choices'][0]["message"]["content"]
+
+        return openai_content
